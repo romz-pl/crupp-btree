e210d3f9dd38dd7f6c0e704e1617546802798761
Zbigniew Romanowski <romz@wp.pl>
2018-05-08 07:49:59 +0200

    Move code of class Page form header to source file

--- src/2page/page.cc
+++ src/2page/page.cc
@@ -18,7 +18,7 @@
 
 #include "0root/root.h"
 
-#include <string.h>
+#include <cstring>
 #include "3rdparty/murmurhash3/MurmurHash3.h"
 
 #include "1base/error.h"
@@ -31,76 +31,326 @@ namespace upscaledb {
 
 uint64_t Page::ms_page_count_flushed = 0;
 
-Page::Page(Device *device, LocalDb *db)
-  : device_(device), db_(db), node_proxy_(0)
+//
+// Default constructor
+//
+Page::Page( Device *device, LocalDb *db )
+    : device_( device )
+    , db_( db )
+    , node_proxy_( nullptr )
 {
-  persisted_data.raw_data = 0;
-  persisted_data.is_dirty = false;
-  persisted_data.is_allocated = false;
-  persisted_data.address  = 0;
-  persisted_data.size     = (uint32_t)device->page_size();
+    persisted_data.raw_data = 0;
+    persisted_data.is_dirty = false;
+    persisted_data.is_allocated = false;
+    persisted_data.address = 0;
+    persisted_data.size = (uint32_t)device->page_size();
 }
 
+//
+// Destructor - releases allocated memory and resources, but neither
+// flushes dirty pages to disk nor moves them to the freelist!
+// Asserts that no cursors are attached.
+//
 Page::~Page()
 {
-  assert(cursor_list.is_empty());
-  free_buffer();
+    assert( cursor_list.is_empty() );
+    free_buffer();
 }
 
-uint32_t
-Page::usable_page_size()
+//
+// Returns the size of the usable persistent payload of a page
+// (page_size minus the overhead of the page header)
+//
+uint32_t Page::usable_page_size()
 {
-  uint32_t raw_page_size = db_->env->config.page_size_bytes;
-  return raw_page_size - Page::kSizeofPersistentHeader;
+    uint32_t raw_page_size = db_->env->config.page_size_bytes;
+    return raw_page_size - Page::kSizeofPersistentHeader;
 }
 
-void
-Page::alloc(uint32_t type, uint32_t flags)
+//
+//
+//
+void Page::alloc(uint32_t type, uint32_t flags)
 {
-  device_->alloc_page(this);
+    device_->alloc_page( this );
 
-  if (flags & kInitializeWithZeroes) {
-    size_t page_size = device_->page_size();
-    ::memset(raw_payload(), 0, page_size);
-  }
-
-  if (type)
-    set_type(type);
-}
-
-void
-Page::fetch(uint64_t address)
-{
-  device_->read_page(this, address);
-  set_address(address);
-}
-
-void
-Page::flush()
-{
-  if (persisted_data.is_dirty) {
-    // update crc32
-    if (IS_SET(device_->config.flags, UPS_ENABLE_CRC32)
-        && likely(!persisted_data.is_without_header)) {
-      MurmurHash3_x86_32(persisted_data.raw_data->header.payload,
-                         persisted_data.size - (sizeof(PPageHeader) - 1),
-                         (uint32_t)persisted_data.address,
-                         &persisted_data.raw_data->header.crc32);
+    if( flags & kInitializeWithZeroes )
+    {
+        size_t page_size = device_->page_size();
+        ::memset( raw_payload(), 0, page_size );
     }
-    device_->write(persisted_data.address, persisted_data.raw_data,
-                    persisted_data.size);
-    persisted_data.is_dirty = false;
-    ms_page_count_flushed++;
-  }
+
+    if( type )
+    {
+        set_type( type );
+    }
 }
 
-void
-Page::free_buffer()
+//
+//
+//
+void Page::fetch( uint64_t address )
 {
-  if (node_proxy_) {
-    delete node_proxy_;
-    node_proxy_ = 0;
-  }
+    device_->read_page( this, address );
+    set_address( address );
 }
+
+//
+//
+//
+void Page::flush()
+{
+    if( persisted_data.is_dirty )
+    {
+        // update crc32
+        if( IS_SET( device_->config.flags, UPS_ENABLE_CRC32 ) && likely( !persisted_data.is_without_header ) )
+        {
+            MurmurHash3_x86_32( persisted_data.raw_data->header.payload,
+                                persisted_data.size - (sizeof(PPageHeader) - 1),
+                                (uint32_t)persisted_data.address,
+                                &persisted_data.raw_data->header.crc32 );
+        }
+        device_->write( persisted_data.address, persisted_data.raw_data, persisted_data.size );
+        persisted_data.is_dirty = false;
+        ms_page_count_flushed++;
+    }
+}
+
+//
+//
+//
+void Page::free_buffer()
+{
+    if( node_proxy_ )
+    {
+        delete node_proxy_;
+        node_proxy_ = 0;
+    }
+}
+
+//
+// Returns the spinlock
+//
+Spinlock& Page::mutex()
+{
+    return persisted_data.mutex;
+}
+
+//
+// Returns the database which manages this page; can be NULL if this
+// page belongs to the Environment (i.e. for freelist-pages)
+//
+LocalDb* Page::db()
+{
+    return db_;
+}
+
+//
+// Sets the database to which this Page belongs
+//
+void Page::set_db( LocalDb *db )
+{
+    db_ = db;
+}
+
+//
+// Returns the address of this page
+//
+uint64_t Page::address() const
+{
+    return persisted_data.address;
+}
+
+//
+// Sets the address of this page
+//
+void Page::set_address( uint64_t address )
+{
+    persisted_data.address = address;
+}
+
+//
+// Returns the page's type (kType*)
+//
+uint32_t Page::type() const
+{
+    return persisted_data.raw_data->header.flags;
+}
+
+//
+// Sets the page's type (kType*)
+//
+void Page::set_type( uint32_t type )
+{
+    persisted_data.raw_data->header.flags = type;
+}
+
+//
+// Returns the crc32
+//
+uint32_t Page::crc32() const
+{
+    return persisted_data.raw_data->header.crc32;
+}
+
+//
+// Sets the crc32
+//
+void Page::set_crc32( uint32_t crc32 )
+{
+    persisted_data.raw_data->header.crc32 = crc32;
+}
+
+//
+// Returns the lsn
+//
+uint64_t Page::lsn() const
+{
+    return persisted_data.raw_data->header.lsn;
+}
+
+//
+// Sets the lsn
+//
+void Page::set_lsn( uint64_t lsn )
+{
+    persisted_data.raw_data->header.lsn = lsn;
+}
+
+//
+// Returns the pointer to the persistent data
+//
+PPageData* Page::data()
+{
+    return persisted_data.raw_data;
+}
+
+//
+// Sets the pointer to the persistent data
+//
+void Page::set_data( PPageData* data )
+{
+    persisted_data.raw_data = data;
+}
+
+//
+// Returns the persistent payload (after the header!)
+//
+uint8_t* Page::payload()
+{
+    return persisted_data.raw_data->header.payload;
+}
+
+//
+// Returns the persistent payload (including the header!)
+//
+uint8_t* Page::raw_payload()
+{
+    return persisted_data.raw_data->payload;
+}
+
+//
+// Returns true if this is the header page of the Environment
+//
+bool Page::is_header() const
+{
+    return persisted_data.address == 0;
+}
+
+//
+// Returns true if this page is dirty (and needs to be flushed to disk)
+//
+bool Page::is_dirty() const
+{
+    return persisted_data.is_dirty;
+}
+
+//
+// Sets this page dirty/not dirty
+//
+void Page::set_dirty( bool dirty )
+{
+    persisted_data.is_dirty = dirty;
+}
+
+//
+// Returns true if the page's buffer was allocated with malloc
+//
+bool Page::is_allocated() const
+{
+    return persisted_data.is_allocated;
+}
+
+//
+// Returns true if the page has no persistent header
+//
+bool Page::is_without_header() const
+{
+    return persisted_data.is_without_header;
+}
+
+//
+// Sets the flag whether this page has a persistent header or not
+//
+void Page::set_without_header( bool is_without_header )
+{
+    persisted_data.is_without_header = is_without_header;
+}
+
+//
+// Assign a buffer which was allocated with malloc()
+//
+void Page::assign_allocated_buffer( void* buffer, uint64_t address )
+{
+    free_buffer();
+    persisted_data.raw_data = (PPageData *)buffer;
+    persisted_data.is_allocated = true;
+    persisted_data.address = address;
+}
+
+//
+// Assign a buffer from mmapped storage
+//
+void Page::assign_mapped_buffer( void* buffer, uint64_t address )
+{
+    free_buffer();
+    persisted_data.raw_data = (PPageData *)buffer;
+    persisted_data.is_allocated = false;
+    persisted_data.address = address;
+}
+
+//
+// Returns the cached BtreeNodeProxy
+//
+BtreeNodeProxy* Page::node_proxy()
+{
+    return node_proxy_;
+}
+
+//
+// Sets the cached BtreeNodeProxy
+//
+void Page::set_node_proxy( BtreeNodeProxy* proxy )
+{
+    node_proxy_ = proxy;
+}
+
+//
+// Returns the next page in a linked list
+//
+Page* Page::next( int list )
+{
+    return list_node.next[ list ];
+}
+
+//
+// Returns the previous page of a linked list
+//
+Page* Page::previous( int list )
+{
+    return list_node.previous[ list ];
+}
+
+
 
 } // namespace upscaledb
--- src/2page/page.h
+++ src/2page/page.h
@@ -19,16 +19,10 @@
 #ifndef UPS_PAGE_H
 #define UPS_PAGE_H
 
-#include <cstring>
 #include <cstdint>
 
-#include "1base/error.h"
 #include "1base/spinlock.h"
-#include "1mem/mem.h"
 #include "1base/intrusive_list.h"
-#include "3btree/btree_cursor.h"
-#include "ppage_header.h"
-#include "ppage_data.h"
 #include "persisted_data.h"
 
 namespace upscaledb {
@@ -39,43 +33,44 @@ struct BtreeNodeProxy;
 struct LocalDb;
 
 
-
-
-
-class Page {
-  public:
+class Page
+{
+public:
 
     // Misc. enums
-    enum {
-      // sizeof the persistent page header
-      kSizeofPersistentHeader = sizeof(PPageHeader) - 1,
+    enum
+    {
+        // sizeof the persistent page header
+        kSizeofPersistentHeader = sizeof( PPageHeader ) - 1,
 
-      // instruct Page::alloc() to reset the page with zeroes
-      kInitializeWithZeroes,
+        // instruct Page::alloc() to reset the page with zeroes
+        kInitializeWithZeroes,
     };
 
     // The various linked lists (indices in m_prev, m_next)
-    enum {
-      // list of all cached pages
-      kListCache              = 0,
+    enum
+    {
+        // list of all cached pages
+        kListCache              = 0,
 
-      // list of all pages in a changeset
-      kListChangeset          = 1,
+        // list of all pages in a changeset
+        kListChangeset          = 1,
 
-      // a bucket in the hash table of the cache
-      kListBucket             = 2,
+        // a bucket in the hash table of the cache
+        kListBucket             = 2,
 
-      // array limit
-      kListMax                = 3
+        // array limit
+        kListMax                = 3
     };
 
     // non-persistent page flags
-    enum {
-      // page->m_data was allocated with malloc, not mmap
-      kNpersMalloc            = 1,
+    enum
+    {
+        // page->m_data was allocated with malloc, not mmap
+        kNpersMalloc            = 1,
 
-      // page has no header (i.e. it's part of a large blob)
-      kNpersNoHeader          = 2
+        // page has no header (i.e. it's part of a large blob)
+        kNpersNoHeader          = 2
     };
 
     // Page types
@@ -83,192 +78,81 @@ class Page {
     // When large BLOBs span multiple pages, only their initial page
     // will have a valid type code; subsequent pages of this blog will store
     // the data as-is, so as to provide one continuous storage space
-    enum {
-      // unidentified db page type
-      kTypeUnknown            =  0x00000000,
+    enum
+    {
+        // unidentified db page type
+        kTypeUnknown            =  0x00000000,
 
-      // the header page: this is the first page in the environment (offset 0)
-      kTypeHeader             =  0x10000000,
+        // the header page: this is the first page in the environment (offset 0)
+        kTypeHeader             =  0x10000000,
 
-      // a B+tree root page
-      kTypeBroot              =  0x20000000,
+        // a B+tree root page
+        kTypeBroot              =  0x20000000,
 
-      // a B+tree node page
-      kTypeBindex             =  0x30000000,
+        // a B+tree node page
+        kTypeBindex             =  0x30000000,
 
-      // a page storing the state of the PageManager
-      kTypePageManager        =  0x40000000,
+        // a page storing the state of the PageManager
+        kTypePageManager        =  0x40000000,
 
-      // a page which stores blobs
-      kTypeBlob               =  0x50000000
+        // a page which stores blobs
+        kTypeBlob               =  0x50000000
     };
 
-    // Default constructor
-    Page(Device *device, LocalDb *db = 0);
 
-    // Destructor - releases allocated memory and resources, but neither
-    // flushes dirty pages to disk nor moves them to the freelist!
-    // Asserts that no cursors are attached.
+    Page( Device *device, LocalDb *db = nullptr );
     ~Page();
 
-    // Returns the size of the usable persistent payload of a page
-    // (page_size minus the overhead of the page header)
+
     uint32_t usable_page_size();
+    Spinlock &mutex();
 
-    // Returns the spinlock
-    Spinlock &mutex() {
-      return persisted_data.mutex;
-    }
 
-    // Returns the database which manages this page; can be NULL if this
-    // page belongs to the Environment (i.e. for freelist-pages)
-    LocalDb *db() {
-      return db_;
-    }
+    LocalDb *db();
+    void set_db( LocalDb *db );
 
-    // Sets the database to which this Page belongs
-    void set_db(LocalDb *db) {
-      db_ = db;
-    }
+    uint64_t address() const;
+    void set_address( uint64_t address );
 
-    // Returns the address of this page
-    uint64_t address() const {
-      return persisted_data.address;
-    }
+    uint32_t type() const;
+    void set_type( uint32_t type );
 
-    // Sets the address of this page
-    void set_address(uint64_t address) {
-      persisted_data.address = address;
-    }
+    uint32_t crc32() const;
+    void set_crc32( uint32_t crc32 );
 
-    // Returns the page's type (kType*)
-    uint32_t type() const {
-      return persisted_data.raw_data->header.flags;
-    }
+    uint64_t lsn() const;
+    void set_lsn( uint64_t lsn );
 
-    // Sets the page's type (kType*)
-    void set_type(uint32_t type) {
-      persisted_data.raw_data->header.flags = type;
-    }
+    PPageData* data();
+    void set_data( PPageData *data );
 
-    // Returns the crc32
-    uint32_t crc32() const {
-      return persisted_data.raw_data->header.crc32;
-    }
+    uint8_t* payload();
+    uint8_t* raw_payload();
 
-    // Sets the crc32
-    void set_crc32(uint32_t crc32) {
-      persisted_data.raw_data->header.crc32 = crc32;
-    }
+    bool is_header() const;
 
-    // Returns the lsn
-    uint64_t lsn() const {
-      return persisted_data.raw_data->header.lsn;
-    }
+    bool is_dirty() const;
+    void set_dirty( bool dirty );
 
-    // Sets the lsn
-    void set_lsn(uint64_t lsn) {
-      persisted_data.raw_data->header.lsn = lsn;
-    }
+    bool is_allocated() const;
+    bool is_without_header() const;
+    void set_without_header( bool is_without_header );
 
-    // Returns the pointer to the persistent data
-    PPageData *data() {
-      return persisted_data.raw_data;
-    }
+    void assign_allocated_buffer( void *buffer, uint64_t address );
+    void assign_mapped_buffer( void *buffer, uint64_t address );
 
-    // Sets the pointer to the persistent data
-    void set_data(PPageData *data) {
-      persisted_data.raw_data = data;
-    }
-
-    // Returns the persistent payload (after the header!)
-    uint8_t *payload() {
-      return persisted_data.raw_data->header.payload;
-    }
-    
-    // Returns the persistent payload (including the header!)
-    uint8_t *raw_payload() {
-      return persisted_data.raw_data->payload;
-    }
-
-    // Returns true if this is the header page of the Environment
-    bool is_header() const {
-      return persisted_data.address == 0;
-    }
-
-    // Returns true if this page is dirty (and needs to be flushed to disk)
-    bool is_dirty() const {
-      return persisted_data.is_dirty;
-    }
-
-    // Sets this page dirty/not dirty
-    void set_dirty(bool dirty) {
-      persisted_data.is_dirty = dirty;
-    }
-
-    // Returns true if the page's buffer was allocated with malloc
-    bool is_allocated() const {
-      return persisted_data.is_allocated;
-    }
-
-    // Returns true if the page has no persistent header
-    bool is_without_header() const {
-      return persisted_data.is_without_header;
-    }
-
-    // Sets the flag whether this page has a persistent header or not
-    void set_without_header(bool is_without_header) {
-      persisted_data.is_without_header = is_without_header;
-    }
-
-    // Assign a buffer which was allocated with malloc()
-    void assign_allocated_buffer(void *buffer, uint64_t address) {
-      free_buffer();
-      persisted_data.raw_data = (PPageData *)buffer;
-      persisted_data.is_allocated = true;
-      persisted_data.address = address;
-    }
-
-    // Assign a buffer from mmapped storage
-    void assign_mapped_buffer(void *buffer, uint64_t address) {
-      free_buffer();
-      persisted_data.raw_data = (PPageData *)buffer;
-      persisted_data.is_allocated = false;
-      persisted_data.address = address;
-    }
-
-    // Free resources associated with the buffer
     void free_buffer();
-
-    // Allocates a new page from the device
-    // |flags|: either 0 or kInitializeWithZeroes
-    void alloc(uint32_t type, uint32_t flags = 0);
-
-    // Reads a page from the device
-    void fetch(uint64_t address);
-
-    // Flushes the page to disk, clears the "dirty" flag
+    void alloc( uint32_t type, uint32_t flags = 0 );
+    void fetch( uint64_t address );
     void flush();
 
-    // Returns the cached BtreeNodeProxy
-    BtreeNodeProxy *node_proxy() {
-      return node_proxy_;
-    }
+    BtreeNodeProxy *node_proxy();
+    void set_node_proxy( BtreeNodeProxy* proxy );
 
-    // Sets the cached BtreeNodeProxy
-    void set_node_proxy(BtreeNodeProxy *proxy) {
-      node_proxy_ = proxy;
-    }
+    Page *next( int list );
+    Page *previous( int list );
 
-    // Returns the next page in a linked list
-    Page *next(int list) {
-      return list_node.next[list];
-    }
-
-    // Returns the previous page of a linked list
-    Page *previous(int list) {
-      return list_node.previous[list];
-    }
+public:
 
     // tracks number of flushed pages
     static uint64_t ms_page_count_flushed;
@@ -277,20 +161,20 @@ class Page {
     PersistedData persisted_data;
 
     // Intrusive linked lists
-    IntrusiveListNode<Page, Page::kListMax> list_node;
+    IntrusiveListNode< Page, Page::kListMax > list_node;
 
     // Intrusive linked btree cursors
-    IntrusiveList<BtreeCursor> cursor_list;
+    IntrusiveList< BtreeCursor > cursor_list;
 
-  private:
+private:
     // the Device for allocating storage
-    Device *device_;
+    Device* device_;
 
     // the Database handle (can be NULL)
-    LocalDb *db_;
+    LocalDb* db_;
 
     // the cached BtreeNodeProxy object
-    BtreeNodeProxy *node_proxy_;
+    BtreeNodeProxy* node_proxy_;
 };
 
 } // namespace upscaledb
